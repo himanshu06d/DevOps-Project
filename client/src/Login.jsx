c05d70c7d5dfe038d375bd55accc062e9a6de83b
himanshu06d <55750386+himanshu06d@users.noreply.github.com>
2022-06-24 22:53:36 +0530

    Update Login.jsx

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -16,7 +16,7 @@ function Login () {
                         <Link to='/LoginH'> <button id ='Hotel2'>Hotel</button> </Link> 
                     </div> 
                     <br /><br />
-                    <input type = 'email' placeholder="Enter your email" name='email'  id="email1"></input><br /><br/>
+                    <input type = 'email' placeholder="Enter ayush" name='email'  id="email1"></input><br /><br/>
                     <input type = 'password' placeholder="Enter your Password" name='password'  id="password1"></input> <br /> <br />
                     {/*<Link to ="" id="forgot"><b>Forgot Password?</b></Link> <br />*/}
                     <button type='submit' data-aos="fade-zoom-in">Login</button>
